9086922ef433f6d0cfa6f4f897c03203116a99f5
Support Vimba boss <moto34091@gmail.com>
2026-05-17 11:12:43 +0200

    Update data.js

--- backend/data.js
+++ backend/data.js
@@ -1,38 +1,11 @@
-const fetch = (...args) =>
-import("node-fetch").then(({ default: fetch }) => fetch(...args));
-
-const API_KEY = process.env.TWELVE_API_KEY;
-
-function formatSymbol(symbol) {
-
-let fixedSymbol = symbol;
-
-const forexPairs = [
-"EURUSD","GBPUSD","USDJPY","USDCHF","USDCAD",
-"AUDUSD","NZDUSD","EURJPY","GBPJPY","EURGBP",
-"EURCHF","EURCAD","EURAUD","AUDJPY","CHFJPY"
-];
-
-if (forexPairs.includes(symbol)) {
-fixedSymbol = symbol.slice(0, 3) + "/" + symbol.slice(3);
-}
-
-if (symbol === "XAUUSD") fixedSymbol = "XAU/USD";
-if (symbol === "BTCUSD") fixedSymbol = "BTC/USD";
-if (symbol === "ETHUSD") fixedSymbol = "ETH/USD";
-
-return fixedSymbol;
-
-}
-
 async function getPrice(symbol) {
 
 try {
 
-const fixedSymbol = formatSymbol(symbol);
+const API_KEY = process.env.TWELVE_API_KEY;
 
 const url =
-`https://api.twelvedata.com/price?symbol=${fixedSymbol}&apikey=${API_KEY}`;
+`https://api.twelvedata.com/price?symbol=${symbol}&apikey=${API_KEY}`;
 
 const response = await fetch(url);
 const data = await response.json();
@@ -49,6 +22,4 @@ return null;
 
 }
 
-module.exports = {
-getPrice
-};
+module.exports = { getPrice };
